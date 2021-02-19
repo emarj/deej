0671b2a950ed27c17f70074f79420137bd1df487
Marco Ronchese <dev@ronche.se>
2021-02-19 10:57:28 +0100

    changing build/config scripts

--- scripts/linux/build-dev.sh
+++ scripts/linux/build-dev.sh
@@ -2,6 +2,7 @@
 
 echo 'Building deej (development)...'
 
+FOLDER="bin"
 # shove git commit, version tag into env
 GIT_COMMIT=$(git rev-list -1 --abbrev-commit HEAD)
 VERSION_TAG=$(git describe --tags --always)
@@ -11,8 +12,9 @@ echo "- gitCommit $GIT_COMMIT"
 echo "- versionTag $VERSION_TAG"
 echo "- buildType $BUILD_TYPE"
 
-go build -o deej-dev -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.versionTag=$VERSION_TAG -X main.buildType=$BUILD_TYPE" ./cmd
+go build -o $FOLDER/deej-dev -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.versionTag=$VERSION_TAG -X main.buildType=$BUILD_TYPE" ./cmd
 if [ $? -eq 0 ]; then
+    cp scripts/misc/default-config_linux.yaml bin/config.yaml
     echo 'Done.'
 else
     echo 'Error: "go build" exited with a non-zero code. Are you running this script from the deej directory?'
